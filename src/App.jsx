bf0020f8f019852bf974ffa2ab9807ffb76f5409
NyFanasina <fjaonasitera@gmail.com>
2024-07-27 05:58:22 +0300

    Setting up tailwind ui and flowbite react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button, TextInput } from 'flowbite-react'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <h1 className='text-center text-rose-600'>Hello Word</h1>
+  )
+}
+
+export default App
